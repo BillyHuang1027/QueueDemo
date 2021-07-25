2fb93870ea4ee020ff56d9f01e6795ca17ea8022
Billy Huang <j28898601@gmail.com>
2021-07-25 17:41:32 +0800

    Queue Demo

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -11,9 +11,52 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        var imagePath = "path \(Int.self)"
+        var queue = QueueArray<String>()
+        
+        for x in 0...5 {
+            imagePath = "path \(x)"
+            queue.addToQueue(imagePath)
+            print("add to queue", queue)
+        }
+        
+        for y in 0...5 {
+            imagePath = "path \(y)"
+            imageProcess()
+            queue.removeFromQueue(imagePath)
+            print("remove from queue", queue)
+        }
     }
-
-
 }
 
+public protocol Queue {
+    associatedtype Element
+    mutating func addToQueue(_ imagePath: Element) -> Bool
+    mutating func removeFromQueue(_ imagePath: Element) -> Element?
+    var isEmpty: Bool { get }
+}
+
+public struct QueueArray<T>: Queue {
+    private var array: [String] = []
+    
+    public init() {}
+    
+    public var isEmpty: Bool {
+        return array.isEmpty
+    }
+    
+    public mutating func addToQueue(_ imagePath: String) -> Bool {
+        array.append(imagePath)
+        return true
+    }
+    
+    public mutating func removeFromQueue(_ imagePath: String) -> String? {
+        let index = array.firstIndex(of: imagePath)
+        return isEmpty ? nil : array.remove(at: index!)
+        //isEmpty = true 時回傳 nil，false 則刪除項目
+    }
+}
+
+func imageProcess() {
+    print("call back")
+}
